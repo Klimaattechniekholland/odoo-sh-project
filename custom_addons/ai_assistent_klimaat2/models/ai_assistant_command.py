a9cf4161b441bec6fe535f3e12ae6030e00a6e68
Klimaattechniekholland <anass@klimaattechniekholland.nl>
2025-05-05 20:33:24 +0200

    Add AI Integration module skeleton for Odoo v18 in custom_addons

--- custom_addons/ai_assistent_klimaat2/models/ai_assistant_command.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields
-
-class AIAssistantCommand(models.Model):
-    _name = 'x_ai_assistant_command'
-    _description = 'AI Assistant Command'
-
-    name = fields.Char(string='Naam', required=True)
-    description = fields.Text(string='Beschrijving')
--- custom_addons/ai_assistent_klimaat2/models/ai_user_context.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-class AIUserContext(models.Model):
-    _name = 'x_ai_user_context'
-    _description = 'AI User Context'
-
-    user_id = fields.Many2one('res.users', string='Gebruiker', default=lambda self: self.env.uid)
-    session_uid = fields.Char(string='Sessienaam of ID')
-    messages = fields.Text(string='Berichten JSON')

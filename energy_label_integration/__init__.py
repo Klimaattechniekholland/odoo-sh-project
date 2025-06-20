3df10ae3822fb2e30d8f345b61fc4bc510aced76
Abdessamad <abdessamad@192.168.11.140>
2025-06-20 01:35:46 +0100

    disable demo data

--- energy_label_integration/__init__.py
+++
@@ -1,6 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import hooks
-from . import models
-#from . import services
-from . import utils
--- energy_label_integration/services/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import bag_api_client
-from . import ep_online_client
-

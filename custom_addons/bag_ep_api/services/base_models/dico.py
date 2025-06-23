d5714a69eace0aab7bc1309000df834b08892a0b
RacketRebel <ruud@pi-con.nl>
2025-06-23 08:52:14 +0200

    Data structure added

--- custom_addons/bag_ep_api/services/base_models/dico.py
+++ custom_addons/bag_ep_api/services/base_models/dico.py
@@ -1,3 +1,71 @@
+from odoo import models, fields
+
+
+class SalesOrder(models.Model):
+	_name = 'custom.sales.order'
+	_description = 'Sales Order'
+	
+	order_number = fields.Char(string = "Order Number")
+	order_date = fields.Date(string = "Order Date")
+	requested_delivery_date = fields.Date(string = "Requested Delivery Date")
+	currency = fields.Char(string = "Currency")
+	
+	buyer_id = fields.Many2one('custom.partner', string = "Buyer", ondelete = 'cascade')
+	seller_id = fields.Many2one('custom.partner', string = "Seller", ondelete = 'cascade')
+	delivery_address_id = fields.Many2one('custom.address', string = "Delivery Address", ondelete = 'cascade')
+	
+	order_line_ids = fields.One2many('custom.sales.order.line', 'sales_order_id', string = "Order Lines")
+
+
+class Partner(models.Model):
+	_name = 'custom.partner'
+	_description = 'Partner Details'
+	
+	gln = fields.Char(string = "GLN")
+	name = fields.Char(string = "Name")
+	address_id = fields.Many2one('custom.address', string = "Address", ondelete = 'cascade')
+	contact_id = fields.Many2one('custom.contact', string = "Contact", ondelete = 'cascade')
+
+
+class Address(models.Model):
+	_name = 'custom.address'
+	_description = 'Address Details'
+	
+	name = fields.Char(string = "Name")  # For Delivery Address Name or similar
+	street = fields.Char(string = "Street")
+	house_number = fields.Char(string = "House Number")
+	postal_code = fields.Char(string = "Postal Code")
+	city = fields.Char(string = "City")
+	country_code = fields.Char(string = "Country Code")
+
+
+class Contact(models.Model):
+	_name = 'custom.contact'
+	_description = 'Contact Details'
+	
+	name = fields.Char(string = "Name")
+	phone = fields.Char(string = "Phone")
+	email = fields.Char(string = "Email")
+
+
+class SalesOrderLine(models.Model):
+	_name = 'custom.sales.order.line'
+	_description = 'Sales Order Line'
+	
+	sales_order_id = fields.Many2one('custom.sales.order', string = "Sales Order", ondelete = 'cascade')
+	line_number = fields.Integer(string = "Line Number")
+	
+	supplier_article_number = fields.Char(string = "Supplier Article Number")
+	ean = fields.Char(string = "EAN")
+	description = fields.Char(string = "Product Description")
+	manufacturer = fields.Char(string = "Manufacturer")
+	
+	quantity = fields.Float(string = "Quantity")
+	unit = fields.Char(string = "Unit")
+	net_price = fields.Float(string = "Net Price")
+	discount = fields.Float(string = "Discount")
+	delivery_date = fields.Date(string = "Delivery Date")
+
 # <?xml version="1.0" encoding="UTF-8"?>
 # <SalesOrder version="5.0" xmlns="http://www.ketenstandaard.nl/salesorder/v5">
 #   <Header>

6995cc94e36a3d9caea09b347a84a3d0d936c483
Abdessamad <abdessamad@Abdessamads-MacBook-Pro.local>
2025-08-13 02:38:32 +0100

    multiproposal quotes & forms 1.3

--- sale_multi_proposal/__manifest__.py
+++ sale_multi_proposal/__manifest__.py
@@ -1,18 +1,18 @@
 {
     'name': 'Sale Multi Proposal',
-    'summary': 'Allow multiple proposals per sale order and prevent partner address loops',
+    'summary': 'Allow multiple proposals per sale order',
     'description': "Adds multi-page quotation and invoice layouts (client info, house photo, system description, kit details, terms), custom fields on Sale Orders and Invoices, QR code support for signing, partner-language email templates, and a dynamic service contract wizard.",
     'author': "Klimaat Techniek Holland B.V.",
     'website': "http://www.klimaattechniekholland.nl",
     'version': '18.0.1.0.0',
     'category': 'Sales',
     'license': 'LGPL-3',
-    'depends': ['sale_management','sale',],
+    'depends': ['sale'],
     'data': [
-        'security/ir.model.access.csv',
-        'views/sale_order_proposition_views.xml',
         'views/sale_order_views.xml',
+        'security/ir.model.access.csv',
     ],
     'installable': True,
+    'auto_install': False,
     'application': False,
 }
--- sale_multi_proposal/models/__init__.py
+++ sale_multi_proposal/models/__init__.py
@@ -1,3 +1,1 @@
-from . import sale_order_proposition
-from . import sale_order
-#from . import res_partner
+from . import sale_proposal
